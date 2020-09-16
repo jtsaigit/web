4172567dbc8045bc7e80c7d17846fa4439339327
jtsaigit <37053714+jtsaigit@users.noreply.github.com>
2020-09-16 12:43:42 -0400

    Create words.py

---
+++ words.py
@@ -0,0 +1,21 @@
+from flask import request, jsonify, Flask
+import itertools
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+user = ""
+
+@app.route('/data')
+def data():
+    # Get the value of userInput (i.e. ?userInput=some-value)
+    userInput = request.args.get('userInput').strip()
+
+    # Validate user input to check if it is a word (no numbers or special characters)
+    if userInput.isalpha():
+        list1 = ["".join(perm) for perm in itertools.permutations(userInput)]
+        return ' '.join(list1)
+    else:
+        return "This is not a word, please try again!"
+
+app.run(host='0.0.0.0')
